8fa281417e9569ada7d21353626de1a595b73d3b
Nathanael <Nathan@MacBook-Pro.local>
2023-12-09 13:59:31 +0700

    added create user api

---
+++ src/pages/api/user/index.ts
@@ -0,0 +1,39 @@
+import { User } from "@/models/user";
+import { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcrypt";
+import { connectToDatabase } from "@/lib/connection";
+
+connectToDatabase();
+
+const salt = bcrypt.genSaltSync(5);
+
+export default async function userHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { username, password } = req.body;
+  switch (req.method) {
+    case "POST":
+      try {
+        const userExists = await User.findOne({ user: username });
+        if (userExists) {
+          return res.status(400).json({ message: "User already exists!" });
+        } else {
+          const hashPassword = await bcrypt.hash(password, salt);
+          const user = await User.create({
+            user: username,
+            password: hashPassword,
+            profile: null,
+            purchases: [],
+          });
+          return res.status(200).json({ user });
+        }
+      } catch (err) {
+        console.error("Error creating user:", err);
+        return res.status(500).json({ message: "Internal Server Error" });
+      }
+      break;
+    default:
+      return res.status(405).json({ message: "Method not allowed" });
+  }
+}
