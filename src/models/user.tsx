b3a306485ac4cc1c22deede7fa7dabea9bd73f01
Nathanael <Nathan@MacBook-Pro.local>
2023-12-09 14:12:57 +0700

    update role at user model

--- src/models/user.tsx
+++ src/models/user.tsx
@@ -3,6 +3,7 @@ import mongoose, { Document, Schema } from "mongoose";
 export interface userInterface extends Document {
   user: string;
   password: string;
+  role: string;
   profile: Schema.Types.ObjectId;
   purchases: Schema.Types.ObjectId[] | object[];
   createdAt: Date;
@@ -12,6 +13,7 @@ const UserSchema = new Schema<userInterface>(
   {
     user: { type: String, required: true },
     password: { type: String, required: true },
+    role: { type: String, default: "user" },
     profile: { type: Schema.Types.ObjectId, ref: "Profile" },
     purchases: [{ type: Schema.Types.ObjectId, ref: "ItemsCart" }],
     createdAt: { type: Date, default: Date.now },
