be125f685aaef94be7b0038dc92058bbf0e70b69
Nathanael <Nathan@MacBook-Pro.local>
2023-12-09 12:26:15 +0700

    change user model and add profile model

---
+++ src/models/profile.tsx
@@ -0,0 +1,26 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface profileInterface extends Document {
+  city: string;
+  pin: number;
+  wallet: number;
+  origin: string | object;
+  createdAt: Date;
+}
+
+const ProfileSchema = new Schema<profileInterface>(
+  {
+    city: { type: String, required: true },
+    pin: { type: Number, required: true },
+    wallet: { type: Number, default: 0 },
+    origin: { type: Schema.Types.ObjectId, ref: "User" },
+    createdAt: { type: Date, default: Date.now },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Profile =
+  mongoose.models.Profile ||
+  mongoose.model<profileInterface>("Profile", ProfileSchema);
