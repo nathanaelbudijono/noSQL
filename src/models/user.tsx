2470c89596276f7afcdd20b76d474cde47c0b881
Nathanael <Nathan@MacBook-Pro.local>
2023-12-10 14:03:09 +0700

    update user model

--- src/models/user.tsx
+++ src/models/user.tsx
@@ -4,8 +4,6 @@ export interface userInterface extends Document {
   user: string;
   password: string;
   role: string;
-  profile: Schema.Types.ObjectId;
-  purchases: Schema.Types.ObjectId[] | object[];
   createdAt: Date;
 }
 
@@ -14,8 +12,6 @@ const UserSchema = new Schema<userInterface>(
     user: { type: String, required: true },
     password: { type: String, required: true },
     role: { type: String, default: "user" },
-    profile: { type: Schema.Types.ObjectId, ref: "Profile" },
-    purchases: [{ type: Schema.Types.ObjectId, ref: "ItemsCart" }],
     createdAt: { type: Date, default: Date.now },
   },
   {
