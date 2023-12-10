ae1009a50b4ff3058baab86b6543ea113cfccdf1
Farrel <farrel22fatih@gmail.com>
2023-12-10 21:43:58 +0700

    update api admin

--- src/pages/api/admin/index.ts
+++ src/pages/api/admin/index.ts
@@ -2,6 +2,7 @@ import { Admin } from "@/models/admin";
 import { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcrypt";
 import { connectToDatabase } from "@/lib/connection";
+import jwt from "jsonwebtoken";
 
 connectToDatabase();
 
@@ -12,6 +13,7 @@ export default async function userHandler(
   res: NextApiResponse
 ) {
   const { username, password } = req.body;
+  const token = req.cookies.token as string;
   switch (req.method) {
     case "POST":
       try {
@@ -30,6 +32,32 @@ export default async function userHandler(
         }
       } catch (err) {
         console.error("Error creating user:", err);
+        return res.status(500).json({ message: "Internal Server Error" });
+      }
+      break;
+      case "GET":
+      try {
+        if (token) {
+          const adminToken = await new Promise((resolve, reject) => {
+            jwt.verify(
+              token.substring(1, token.length - 1),
+              process.env.NEXT_PUBLIC_TOKEN_SECRET as string,
+              {},
+              (err, token) => {
+                if (err) {
+                  reject(err);
+                } else {
+                  resolve(token);
+                }
+              }
+            );
+          });
+          return res.status(200).json({ adminToken });
+        } else {
+          return;
+        }
+      } catch (err) {
+        console.error("Error getting user:", err);
         return res.status(500).json({ message: "Internal Server Error" });
       }
       break;
