0db019c1a70d64194faaa5cadc935ff3c9fa8ff9
Farrel <farrel22fatih@gmail.com>
2023-12-17 21:38:44 +0700

    Update admin login

---
+++ src/pages/api/public/admin/login.ts
@@ -0,0 +1,60 @@
+import { Admin } from "@/models/admin";
+import { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcrypt";
+import { connectToDatabase } from "@/lib/connection";
+import jwt from "jsonwebtoken";
+import { serialize } from "cookie";
+
+connectToDatabase();
+
+export default async function userLoginHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { email, password } = req.body;
+  console.log(req.body);
+  switch (req.method) {
+    case "POST":
+      try {
+        const adminDoc = await Admin.findOne({ email: email });
+        const correctPassword = await bcrypt.compare(
+          password,
+          adminDoc.password
+        );
+        console.log("correctPassword", correctPassword);
+        if (correctPassword) {
+          const token = await new Promise((resolve, reject) => {
+            jwt.sign(
+              { email: email, role: adminDoc.role, id: adminDoc._id },
+              process.env.NEXT_PUBLIC_TOKEN_SECRET as string,
+              {},
+              (err, token) => {
+                if (err) {
+                  reject(err);
+                } else {
+                  resolve(token);
+                }
+              }
+            );
+          });
+          const serializedData = serialize("token", JSON.stringify(token), {
+            httpOnly: true,
+            secure: false,
+            path: "/",
+            sameSite: "strict",
+          });
+          res.setHeader("Set-Cookie", serializedData);
+          res.status(200).json({ token });
+        } else {
+          return res
+            .status(400)
+            .json({ message: "Invalid username or password!" });
+        }
+      } catch (err) {
+        console.error("Error logging in:", err);
+        return res.status(500).json({ message: "Internal Server Error!" });
+      }
+    default:
+      return res.status(405).json({ message: "Method not allowed!" });
+  }
+}
