1e26c159c1aa6286ffb6289aa987550478b0ad04
Nathanael <Nathan@MacBook-Pro.local>
2023-12-17 15:02:00 +0700

    change email to unique on user schema

--- src/models/user.tsx
+++ src/models/user.tsx
@@ -18,7 +18,7 @@ const UserSchema = new Schema<userInterface>(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     phoneNumber: { type: String, required: true },
     dob: { type: String, required: true },
     address: { type: String, required: true },
